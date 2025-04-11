8eb60e1187ee7c070d9bd059fbf00628b45209b2
Ricardo Racki <ricardo_racki@hotmail.com>
2025-04-11 15:14:38 -0300

    style: implements new variants

--- src/components/separato.tsx
+++ src/components/separato.tsx
@@ -1,14 +1,24 @@
 import { cn } from "@/utils/cn";
 import { type HTMLAttributes } from "react";
 
+export type SeparatorProps = HTMLAttributes<HTMLDivElement> & {
+  horizontal?: boolean;
+};
+
 export const Separator = ({
   className,
+  horizontal = false,
   ...props
-}: HTMLAttributes<HTMLDivElement>) => (
+}: SeparatorProps) => (
   <div
     className={cn(
-      "w-full bg-zinc-600 h-0.5 roudend-full mb-6 mt-16",
+      "bg-zinc-600 rounded-full opacity-50",
+      {
+        "w-full h-[1px] mt-6 mb-3": !horizontal,
+        "w-[1px] h-full mx-6": horizontal,
+      },
       className
     )}
+    {...props}
   />
 );
